d9709a3fabab7dbf87ef7c349efe68981048f93f
Bogdan <crewdown@yandex.ru>
2024-05-11 20:12:35 +0300

    !

--- src/test/java/ContextPocTests.java
+++ src/test/java/ContextPocTests.java
@@ -1,15 +1,40 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.DefaultConfiguration;
+import org.apache.logging.log4j.core.osgi.BundleContextSelector;
+import org.apache.logging.log4j.core.selector.ClassLoaderContextSelector;
 import org.junit.jupiter.api.Test;
 
+import javax.naming.Context;
 import java.io.File;
+import java.lang.module.Configuration;
 import java.net.URI;
 
 public class ContextPocTests {
     @Test
-    public void getContextPocTest(){
+    public void getContextLogManagerTest(){
         URI configUri = new File("config.json").toURI();
         LoggerContext context = (LoggerContext) LogManager.getContext(null, false, configUri);
     }
+
+    @Test
+    public void getContextClassLoaderContextSelectorTest(){
+        ClassLoaderContextSelector contextSelector = new ClassLoaderContextSelector();
+        URI configUri = new File("config.json").toURI();
+        LoggerContext context = contextSelector.getContext( ClassLoaderContextSelector.class.getName() ,null, false, configUri );
+        context.setConfigLocation(null);
+        contextSelector.getContext( ClassLoaderContextSelector.class.getName() ,null, false, configUri );
+    }
+
+    @Test
+    public void getContextBundleContextSelectorTest(){
+       BundleContextSelector contextSelector = new BundleContextSelector();
+       URI configUri = new File("config.json").toURI();
+       LoggerContext context = contextSelector.getContext( BundleContextSelector.class.getName() ,null, false, configUri );
+        context.setConfigLocation(null);
+        contextSelector.getContext( BundleContextSelector.class.getName() ,null, false, configUri );
+    }
+
+
 
 }
